45633c7c412d037d0933eced7ad71524a4fab6f5
spencerDo <33916209+spencerDo@users.noreply.github.com>
2022-07-05 21:57:00 +0900

    Create 네트워크.py

---
+++ 네트워크.py
@@ -0,0 +1,18 @@
+def solution(n, computers):
+    def dfs(x,y):
+        if x < 0 or y < 0 or x >= n or y >= n:
+            return False
+        if computers[x][y] == 1:
+            computers[x][y] = 0
+            dfs(x,y-1)
+            dfs(x,y+1)
+            dfs(x+1,y)
+            dfs(x-y,y)
+            return True
+        return False
+    answer = 0
+    for i in range(n):
+        for j in range(n):
+            if dfs(i,j) == True:
+                answer += 1
+    return answer
