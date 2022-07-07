95ba2855e755f94acc0af3e3f7e98b25ceb5704e
spencerDo <33916209+spencerDo@users.noreply.github.com>
2022-07-07 20:58:01 +0900

    Create 미로탈출.py

---
+++ 미로탈출.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+# n, m = map(int, input().split())
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int, input())))
+n=5
+m=6
+graph = [
+    [1,0,1,0,1,0],
+    [1,1,1,1,1,1],
+    [0,0,0,0,0,1],
+    [1,1,1,1,1,1],
+    [1,1,1,1,1,1]
+]
+
+#상하좌우 이동
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 좌표 범위 넘어갈때
+            if nx <= -1 or nx >= n or ny <= -1 or ny >= m:
+                continue
+            # 지나갈 수 없는 경로일때
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+    return graph[n-1][m-1]
+
+print(bfs(0,0))
