806ed433a680a71dfcd35887b5b97c58abdf005e
spencerDo <33916209+spencerDo@users.noreply.github.com>
2022-07-15 00:34:14 +0900

    Create 3_괄호변환.py

---
+++ 3_괄호변환.py
@@ -0,0 +1,26 @@
+def solution(p):
+    if len(p) == 0:
+        return p
+    else:
+        u = ''
+        v = ''
+        count = 0
+        for i in p:
+            u += i
+            if i == '()':
+                count += 1
+            else:
+                count -= 1
+            if count == 0:
+                v = p[len(u):]
+                break
+    U = u[:]
+    while(U):
+        a = len(U)
+        U = U.replace("()","")
+        if a == len(U):
+            break
+    else:
+        return u + solution(v)
+
+    return "(" + solution(v) + ")" + u[1:-1][::-1]
